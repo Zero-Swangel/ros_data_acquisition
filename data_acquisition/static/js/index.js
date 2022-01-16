438dee4d3249095ad3447111b6643206613c4e3d
Walker <zero.stevewalker@gmail.com>
2022-01-16 17:28:29 +0800

    a simple front-end

--- data_acquisition/static/js/index.js
+++ data_acquisition/static/js/index.js
@@ -1,40 +1,66 @@
 $(function () {
-    setInterval(function (){
+    const $speed_box = $(".speed_box");
+    const $current_pose = $(".current_pose");
+    const $pandar_points = $(".pandar_points");
+    const $lidar_output = $(".lidar_output");
+    const $command = $(".command");
+    setInterval(function () {
         $.get("/get_ros", function (data, status) {
             const obj = jQuery.parseJSON(data);
-            if(obj["position"] === false){
-                $(".speed_box").css("background-color", "red");
-                $(".current_pose").css("background-color", "red");
-            }else{
-                $(".speed_box").css("background-color", "cadetblue");
-                $(".current_pose").css("background-color", "cadetblue");
-                $(".speed_box").text(obj["position"]["data"][2]);
-                $(".current_pose").text(obj["position"]["data"][0]+", "+obj["position"]["data"][1]);
+            if (obj["position"] === false) {
+                $speed_box.addClass("emergency");
+                $current_pose.addClass("emergency");
+            } else {
+                $speed_box.removeClass("emergency");
+                $current_pose.removeClass("emergency");
+                $speed_box.children("div").text(obj["position"]["data"][2]);
+                $current_pose.children("div").text(obj["position"]["data"][0] + ", " + obj["position"]["data"][1]);
+            }
+            if (obj["cloud"] === false) {
+                $pandar_points.addClass("emergency");
+            } else {
+                $pandar_points.removeClass("emergency");
+                $pandar_points.children("div").text(obj["cloud"]["data"]);
+            }
+            if (obj["lidar"] === false) {
+                $lidar_output.addClass("emergency");
+            } else {
+                $lidar_output.removeClass("emergency");
+                $lidar_output.children("div").text(obj["lidar"]["data"]);
+            }
+            if (obj["command"] === false) {
+                $command.addClass("emergency");
+            } else {
+                $command.removeClass("emergency");
+                // $command.children("li").html(obj["command"]["data"]);
+                $command.children("ul").children("li").each(function() {
+                    $(this).text(obj["command"]["data"][$(this).index()].toFixed(2))
+                });
             }
         });
     }, 500);
 
-    setInterval(function (){
+    setInterval(function () {
         $.get("/get_sys", function (data, status) {
             const obj = jQuery.parseJSON(data);
-            $(".cpu_box").text(obj["cpu"]+"%");
-            $(".ram_box").text(obj["used_mem"]+"/"+obj["total_mem"]);
+            $(".cpu_box div").text(obj["cpu"] + "%");
+            $(".ram_box div").html(obj["used_mem"] + "<br />/" + obj["total_mem"]);
         });
     }, 3000);
 
-    $(".topic_tag").on("click", function (){
+    $(".topic_tag").on("click", function () {
         $(".topic_box").css("opacity", 1);
         $(".terminal_box").css("opacity", 0);
     });
 
-    $(".terminal_tag").on("click", function (){
+    $(".terminal_tag").on("click", function () {
         $(".terminal_box").css("opacity", 1);
         $(".topic_box").css("opacity", 0);
     });
 });
 
-$(window).resize(function() {
-    if($(".topic_tag").width()+$(".terminal_tag").width()>$(".topics").width()) {
+$(window).resize(function () {
+    if ($(".topic_tag").width() + $(".terminal_tag").width() > $(".topics").width()) {
         $(".topics").css("grid-template-columns", "30vh 30vh auto");
     }
 });
--- data_acquisition/views/modules/topics.py
+++ data_acquisition/views/modules/topics.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python3.8
 
-import rospy, json, time
+import rospy
 from geometry_msgs.msg import PoseStamped
+from sensor_msgs.msg import PointCloud2
+from std_msgs.msg import Float64MultiArray
+from .msg._LidarOutput import LidarOutput
+from jsk_recognition_msgs.msg import BoundingBoxArray
 
 
 class TopicMonitor:
 
     def __init__(self):
         rospy.init_node('topic_monitor', anonymous=True, disable_signals=True)
-        rospy.Subscriber("/current_pose", PoseStamped, self.pose_callback)
+        rospy.Subscriber('/current_pose', PoseStamped, self.pose_callback)
+        rospy.Subscriber('/pandar_points', PointCloud2, self.point_callback)
+        rospy.Subscriber('/lidar_output', LidarOutput, self.lidar_callback)
+        rospy.Subscriber('/command', Float64MultiArray, self.command_callback)
 
         self.res = {}
         self.position = {'timestamp': -99999, 'data': []}
+        self.cloud = {'timestamp': -99999, 'data': 0}
+        self.command = {'timestamp': -99999, 'data': []}
+        self.lidar = {'timestamp': -99999, 'data': 0}
 
     def get(self):
         now_time = rospy.Time.now().to_sec()
@@ -21,6 +31,21 @@ class TopicMonitor:
         else:
             self.res['position'] = self.position
 
+        if now_time - self.cloud['timestamp'] > 1:
+            self.res['cloud'] = False
+        else:
+            self.res['cloud'] = self.cloud
+
+        if now_time - self.lidar['timestamp'] > 1:
+            self.res['lidar'] = False
+        else:
+            self.res['lidar'] = self.lidar
+
+        if now_time - self.command['timestamp'] > 1:
+            self.res['command'] = False
+        else:
+            self.res['command'] = self.command
+
         return self.res
 
     def pose_callback(self, data):
@@ -29,6 +54,17 @@ class TopicMonitor:
                                   round(data.pose.position.y, 2),
                                   round(data.pose.orientation.w, 2)]}
 
+    def point_callback(self, data):
+        self.cloud = {'timestamp': data.header.stamp.to_sec(),
+                      'data': data.height * data.width}
+
+    def command_callback(self, data):
+        self.command = {'timestamp': rospy.Time.now().to_sec(),
+                        'data': data.data}
+
+    def lidar_callback(self, data):
+        self.lidar = {'timestamp': data.header.stamp.to_sec(),
+                      'data': len(data.global_bounding_box_array.boxes)}
 
 # if __name__ == '__main__':
 #     obj = TopicMonitor()
